28d0a6956d991dd7017567ec4557d038eabafc41
EndermanbugZJFC <175898536+Ezjfc@users.noreply.github.com>
2025-11-23 04:31:42 +1100

    Add(lib): core implementations.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,91 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! Util macro to attach read-only metadata to structs in the parent module.
+
+/// The main struct can be defined in whatever way desired.
+/// But all fields in each metadata struct will always be public.
+///
+/// This macro rule receives a main struct and multiple metadata structs to recursively copy-paste
+/// the same field names for each of the structs in the macro below.
+#[macro_export]
+macro_rules! metadata {
+    ($main_struct:tt, {
+        $(
+            $(#[$metadata_attrs:meta])*
+            $metadata_vis:vis
+            struct $metadata_struct:ident: $metadata_type:ty;
+        )+
+    }) => {
+        $(
+            use put_struct;
+            put_struct!($main_struct);
+            metadata_single!(
+                $main_struct,
+                $(#[$metadata_attrs])* $metadata_vis struct $metadata_struct: $metadata_type
+            );
+        )+
+    };
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// The main struct can be defined in whatever way desired.
+/// But all fields in the metadata struct will always be public.
+///
+/// This macro receives a main struct and a metadata struct. The metadata struct will takeover all
+/// fields in the main struct but replace their types to the given one:
+#[macro_export]
+macro_rules! metadata_single {
+    (
+        {
+            $(#[$attrs:meta])*
+            $vis:vis
+            struct $name:ident {
+                $(
+                    $(#[$field_attrs:meta])*
+                    $field_vis:vis
+                    $field:ident: $type:ty
+                ),*
+                $(,)?
+            }
+        },
+        $(#[$metadata_attrs:meta])*
+        $metadata_vis:vis
+        struct $metadata_struct:ident: $metadata_type:ty
+    ) => {
+        $(#[$metadata_attrs])*
+        $metadata_vis
+        struct $metadata_struct {
+            $(
+                $(#[$field_attrs])*
+                $field_vis
+                $field: $metadata_type
+            )*
+        }
+    };
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// This rule is almost no-op except maintaining the macro hygigene of Rust.
+/// See: https://stackoverflow.com/a/75530574/13787084
+/// It receives the main struct and pastes it:
+#[allow(unused_macros)]
+macro_rules! put_struct {
+    ({
+        $(#[$attrs:meta])*
+        $vis:vis
+        struct $name:ident {
+            $(
+                $(#[$field_attrs:meta])*
+                $field_vis:vis
+                $field:ident: $type:ty
+            ),*
+            $(,)?
+        }
+    }) => {
+        $(#[$attrs])*
+        $vis
+        struct $name {
+            $(
+                $(#[$field_attrs])*
+                $field_vis
+                $field: $type
+            )*
+        }
+    };
 }
